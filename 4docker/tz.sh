19437ac196d6b38c6643f221f0c6e756eed2a213
MikEarpp <72660689+MikEarpp@users.noreply.github.com>
2026-04-05 08:58:12 +0200

    Enhance tz.sh to install tzdata if missing
    Add conditional installation of tzdata based on package manager

--- 4docker/tz.sh
+++ 4docker/tz.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
-ln -fs /usr/share/zoneinfo/Europe/Paris /etc/localtime
+set -e
+[ -f /usr/share/zoneinfo/Europe/Paris ] || {
+    if [ "$1" = "apt" ]; then
+        apt-get update \
+        && apt-get install -y --no-install-recommends tzdata \
+        && rm -rf /var/lib/apt/lists/*
+    elif [ "$1" = "apk" ]; then
+        apk add --no-cache tzdata
+    else
+        exit 1
+    fi
+}
 echo "Europe/Paris" > /etc/timezone
+ln -fs /usr/share/zoneinfo/Europe/Paris /etc/localtime
